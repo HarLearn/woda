47f9d33e29e1c96b2f80c350f954eff25c4ff2ae
CrazyIdeas <576445357@qq.com>
2020-08-19 16:48:50 +0800

    初始化

---
+++ src/main/java/com/woda/run/WoDaApplication.java
@@ -0,0 +1,13 @@
+package com.woda.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoDaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoDaApplication.class, args);
+    }
+
+}
